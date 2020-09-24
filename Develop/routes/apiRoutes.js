360c79e7632ed0a9a28f750173a07e5840d89816
kpegeder <k.pegeder@gmail.com>
2020-09-23 22:39:20 -0700

    changing routing for reading and writing

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -14,44 +14,48 @@ const fs = require("fs");
 module.exports = function (app) {
   // API GET Requests
   app.get("/api/notes", function (req, res) {
-    let rawNotes = fs.readFileSync("db/db.json");
+    let rawNotes = fs.readFileSync("./Develop/db/db.json");
     let note = JSON.parse(rawNotes);
     res.json(note);
   });
-
   // API POST Requests
   // Create a note
   app.post("/api/notes", function (req, res) {
     const { v4: uuidv4 } = require("uuid");
     let newNote = req.body;
-    let rawNotes = fs.readFileSync("db/db.json");
+    let rawNotes = fs.readFileSync("./Develop/db/db.json");
     let notes = JSON.parse(rawNotes);
-
     newNote.id = uuidv4();
     notes.push(newNote);
-
-    fs.writeFile("db/db.json", JSON.stringify(notes), "utf8", (err) => {
-      if (err) throw err;
-      res.json(notes);
-    });
+    fs.writeFile(
+      "./Develop/db/db.json",
+      JSON.stringify(notes),
+      "utf8",
+      (err) => {
+        if (err) throw err;
+        res.json(notes);
+      }
+    );
   });
-
   // Deleting a Note
   app.delete("/api/notes/:id", function (req, res) {
     let noteID = req.params.id;
-    let rawNotes = fs.readFileSync("db/db.json");
+    let rawNotes = fs.readFileSync("./Develop/db/db.json");
     let notes = JSON.parse(rawNotes);
-
     for (let i = 0; i < notes.length; i++) {
       if (notes[i].id === noteID) {
         notes.splice(i, 1);
         console.log("Delete this");
       }
     }
-
-    fs.writeFile("db/db.json", JSON.stringify(notes), "utf8", (err) => {
-      if (err) throw err;
-      res.send("Deleted Note");
-    });
+    fs.writeFile(
+      "./Develop/db/db.json",
+      JSON.stringify(notes),
+      "utf8",
+      (err) => {
+        if (err) throw err;
+        res.send("Deleted Note");
+      }
+    );
   });
 };
