1d7ae2326c4f301d72d185637f62d72b0e626300
kpegeder <k.pegeder@gmail.com>
2020-09-23 22:24:48 -0700

    change static route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
 
 // Routes
 // ===============================================
